180d4f1282623b875307d5f29eda120c9276f00e
NataliiaBoh <homesweethome160126@gmail.com>
2024-03-25 17:01:49 +0200

    add  js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,40 @@
-console.log("Form");
+const LS_KEY = "feedback-form-state";
+
+
+const formEl = document.querySelector(".feedback-form");
+const inputEmeil = formEl.elements.email;
+const inputMessage = formEl.querySelector("textarea");
+
+
+formEl.addEventListener('submit', handleSubmit);
+formEl.addEventListener('input', handleInput);
+
+populateFormEl();
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log({email: inputEmeil.value.trim(), message: inputMessage.value.trim()});
+
+    event.currentTarget.reset();
+    localStorage.removeItem(LS_KEY)
+} 
+        
+
+function handleInput() {
+    const formValues = {email: inputEmeil.value.trim(), message: inputMessage.value.trim()};
+    localStorage.setItem(LS_KEY, JSON.stringify(formValues));
+
+}
+
+
+function populateFormEl() {
+    const savedValues = localStorage.getItem(LS_KEY);
+    if (savedValues) {
+            const { email, message } = JSON.parse(savedValues);
+            inputEmeil.value = email;
+            inputMessage.value = message;
+
+}
+}
